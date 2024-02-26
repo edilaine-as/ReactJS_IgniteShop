3e0dbbf3014d0b56566a52137a6f34a38e9a4681
Edilaine Alves dos Santos <91744159+edilaine-as@users.noreply.github.com>
2024-02-26 15:19:15 -0300

    Working on checkout and success api

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -4,9 +4,11 @@ import {
   ProductContainer,
   ProductDetails,
 } from '@/src/styles/pages/product'
+import axios from 'axios'
 import { GetStaticProps } from 'next'
 import Image from 'next/image'
 import { useRouter } from 'next/router'
+import { useState } from 'react'
 import Stripe from 'stripe'
 
 interface ProductProps {
@@ -16,14 +18,36 @@ interface ProductProps {
     imageUrl: string
     price: string
     description: string
+    defaultPriceId: string
   }
 }
 
 export default function Product({ product }: ProductProps) {
-  const { isFallback } = useRouter()
+  const [isCreatingCheckoutSession, setIsCreatingCheckoutSession] =
+    useState(false)
 
-  if (isFallback) {
-    return <p>Loading...</p>
+  const router = useRouter()
+
+  async function handleBuyProduct() {
+    try {
+      setIsCreatingCheckoutSession(true)
+
+      const response = await axios.post('/api/checkout', {
+        priceId: product.defaultPriceId,
+      })
+
+      const { checkoutUrl } = response.data
+
+      router.push('/checkout')
+
+      window.location.href = checkoutUrl
+    } catch (err) {
+      // conectar com uma ferramenta de observabilidade (Datalog/Sentry)
+
+      setIsCreatingCheckoutSession(false)
+
+      alert('Falha ao redirecionar ao checkout!')
+    }
   }
 
   return (
@@ -37,7 +61,9 @@ export default function Product({ product }: ProductProps) {
 
         <p>{product.description}</p>
 
-        <button>Comprar agora</button>
+        <button disabled={isCreatingCheckoutSession} onClick={handleBuyProduct}>
+          Comprar agora
+        </button>
       </ProductDetails>
     </ProductContainer>
   )
@@ -75,6 +101,7 @@ export const getStaticProps: GetStaticProps<any, { id: string }> = async ({
           currency: 'BRL',
         }).format(price.unit_amount! / 100),
         description: product.description,
+        defaultPriceId: price.id,
       },
     },
     revalidate: 60 * 60 * 1,
